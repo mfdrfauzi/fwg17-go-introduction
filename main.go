1c3951faae39e7751b0cc2093af25f386ddf7bd9
mfdrfauzi <etakumi.zzy@gmail.com>
2024-01-26 13:10:32 +0700

    Change filenames to tasks 1 2 3

--- main.go
+++ main.go
@@ -2,39 +2,38 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
 	//Print Segitiga
 
-	fmt.Println("---Print Segitiga Terbalik---")
-	printSegitiga(12)
+	// fmt.Println("---Print Segitiga Terbalik---")
+	// printSegitiga(12)
 
 	//Generate Password
-	fmt.Println("---Generate Password---")
-	var password, level string
+	// fmt.Println("---Generate Password---")
+	// var password, level string
 
-	fmt.Print("Enter password: ")
-	fmt.Scan(&password)
+	// fmt.Print("Enter password: ")
+	// fmt.Scan(&password)
 
-	for {
-		fmt.Print("Enter level (low, med, strong): ")
-		fmt.Scan(&level)
+	// for {
+	// 	fmt.Print("Enter level (low, med, strong): ")
+	// 	fmt.Scan(&level)
 
-		if strings.ToLower(level) == "low" || strings.ToLower(level) == "med" || strings.ToLower(level) == "strong" {
-			break
-		} else {
-			fmt.Println("Invalid level. Please enter 'low', 'med', or 'strong'.")
-		}
-	}
+	// 	if strings.ToLower(level) == "low" || strings.ToLower(level) == "med" || strings.ToLower(level) == "strong" {
+	// 		break
+	// 	} else {
+	// 		fmt.Println("Invalid level. Please enter 'low', 'med', or 'strong'.")
+	// 	}
+	// }
 
-	result := genPass(password, level)
-	if result != "" {
-		fmt.Println(result) //perlu / tidak vlidasi di sini
-	}
+	// result := genPass(password, level)
+	// if result != "" {
+	// 	fmt.Println(result) //perlu / tidak vlidasi di sini
+	// }
 
-	//Movie Duration
+	// //Movie Duration
 	fmt.Println("---Find Movie---")
 	data := []int{3, 5, 6, 7, 2} //slice
 
